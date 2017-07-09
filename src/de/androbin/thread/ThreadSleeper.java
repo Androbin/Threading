c66395004a8d403b3b806fba3faf454493e98cae
Androbin <robin.richtsfeld@gmail.com>
2017-07-09 19:58:24 +0200

    Improve time unit granularity

--- src/de/androbin/thread/ThreadSleeper.java
+++ src/de/androbin/thread/ThreadSleeper.java
@@ -1,13 +1,15 @@
 package de.androbin.thread;
 
 public final class ThreadSleeper {
+  private int mikrosLeft;
   private int nanosLeft;
   
   public void reset() {
+    mikrosLeft = 0;
     nanosLeft = 0;
   }
   
-  public void sleep( final long millis ) {
+  public void sleepMilli( final long millis ) {
     if ( millis <= 0L ) {
       return;
     }
@@ -18,11 +20,21 @@ public final class ThreadSleeper {
     }
   }
   
-  public void sleep( final long millis, final int nanos ) {
-    nanosLeft += nanos;
-    final long millisToWait = millis + nanosLeft / 1000000;
-    nanosLeft %= 1000000;
+  public void sleepMikro( final long millis, final int mikros ) {
+    mikrosLeft += mikros;
     
-    sleep( millisToWait );
+    sleepMilli( millis + mikrosLeft / 1000 );
+    
+    mikrosLeft %= 1000;
+  }
+  
+  public void sleepNano( final long millis, final int mikros, final int nanos ) {
+    nanosLeft += nanos;
+    mikrosLeft += mikros + nanosLeft / 1000;
+    
+    sleepMilli( millis + mikrosLeft / 1000 );
+    
+    mikrosLeft %= 1000;
+    nanosLeft %= 1000;
   }
 }
